f93d0638d4086e3defa4b97c774d25d96ad89311
Felipe Cesar Maccari <felipecesarmaccari@gmail.com>
2023-03-07 22:55:51 -0300

    feat(server): add initial base to test user

---
+++ server/src/main/java/br/edu/utfpr/pb/pw25s/server/model/User.java
@@ -0,0 +1,15 @@
+package br.edu.utfpr.pb.pw25s.server.model;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class User {
+
+    private String username;
+    private String displayName;
+    private String password;
+}
