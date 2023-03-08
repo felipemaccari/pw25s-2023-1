16786994e7c81087f75a24940014407f73a86a13
Felipe Cesar Maccari <felipecesarmaccari@gmail.com>
2023-03-07 22:35:41 -0300

    feat(client): add basic routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import Login from "./pages/login";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ client/src/pages/login.tsx
@@ -0,0 +1,5 @@
+const Login = () => {
+  return <div>loginzera</div>;
+};
+
+export default Login;
